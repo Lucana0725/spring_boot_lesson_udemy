134a834cbc4e34d1d73f640ffbfa9a3f72c9b21e
Lucana <k.ark.scarlet72@gmail.com>
2023-06-30 13:19:09 +0900

    [Finish]demoパッケージの中にMainクラスを作成、mainメソッドの中でMemberServiceImplを実行できるように実装

---
+++ src/service/MemberServiceImpl.java
@@ -0,0 +1,34 @@
+package service;
+
+import java.util.ArrayList;
+
+import entity.Member;
+
+public class MemberServiceImpl implements MemberService {
+  
+  @Override
+  // iの値によって挨拶を変更する
+  public String greet(int i) {
+    String[] greetings = {"Good morning", "Hello", "Good evening"};
+    return greetings[i];
+  }
+
+  @Override
+  // listというリストにメンバーを入れていく
+  public ArrayList<Member> getAll() {
+    // 空のリスト(list)を作成
+    ArrayList<Member> list = new ArrayList<>();
+
+    // Memberのインスタンスを作成
+    Member mem1 = new Member(1, "Linda", "linda@example.com");
+    Member mem2 = new Member(2, "James", "james@example.com");
+
+    // listにメンバーを入れる
+    list.add(mem1);
+    list.add(mem2);
+
+    // listを返す
+    return list;
+  }
+
+}
